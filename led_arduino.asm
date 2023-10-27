17705364a52b9aeac9c4fe9e6168dfc39e4531f1
Victor <139719863+victor0989@users.noreply.github.com>
2023-10-27 04:03:56 +0200

    Update led_arduino.asm

--- led_arduino.asm
+++ led_arduino.asm
@@ -35,3 +35,25 @@ _w0:
    pop r17          ; Pop the latest pushed r17 before returning from function
    pop r16          ; Pop the latest pushed r16 before returning from function
    ret              ; Return from the function
+
+; ====================================================
+; DLL-Injection-with-Assembly Copyright(C) 2017 Furkan Türkal
+; This program comes with ABSOLUTELY NO WARRANTY; This is free software,
+; and you are welcome to redistribute it under certain conditions; See
+; file LICENSE, which is part of this source code package, for details.
+; ====================================================
+
+; Example	: Activate the LED if button pressed
+
+; Ports		: LED		-> PORTB's 5th pin
+ 
+.org 0
+	rjmp main
+	
+main:
+	ldi r16, 0x20	; Set PORTB's 5th pin to output mode [0x20 = 0010 0000, r16 = PORTB]
+	out DDRB, r16	; Write the r16's value at the PORTB's DDRB (DataDirectionRegister)
+   
+loop_main:
+	sbi PORTB, 5	; Set PORTB's 5th pin to logic 1 and activate the LED
+	rjmp loop_main	; Go to loop_button tag via RelativeJump and make it work in an infinite loop
