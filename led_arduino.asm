2495c9b180063f3df4238660c7e15361a6b32c6c
Victor <139719863+victor0989@users.noreply.github.com>
2023-10-27 04:04:30 +0200

    Update led_arduino.asm

--- led_arduino.asm
+++ led_arduino.asm
@@ -36,12 +36,7 @@ _w0:
    pop r16          ; Pop the latest pushed r16 before returning from function
    ret              ; Return from the function
 
-; ====================================================
-; DLL-Injection-with-Assembly Copyright(C) 2017 Furkan Türkal
-; This program comes with ABSOLUTELY NO WARRANTY; This is free software,
-; and you are welcome to redistribute it under certain conditions; See
-; file LICENSE, which is part of this source code package, for details.
-; ====================================================
+
 
 ; Example	: Activate the LED if button pressed
 
